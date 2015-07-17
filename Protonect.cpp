957ef655f3df7fb6aae4dcd9488cf8001787f146
Sven Hecht <sven.hecht@gmail.com>
2015-07-18 01:40:09 +0200

    added protonect code

---
+++ Protonect.cpp
@@ -0,0 +1,228 @@
+/*
+ * This file is part of the OpenKinect Project. http://www.openkinect.org
+ *
+ * Copyright (c) 2011 individual OpenKinect contributors. See the CONTRIB file
+ * for details.
+ *
+ * This code is licensed to you under the terms of the Apache License, version
+ * 2.0, or, at your option, the terms of the GNU General Public License,
+ * version 2.0. See the APACHE20 and GPL2 files for the text of the licenses,
+ * or the following URLs:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * If you redistribute this file in source form, modified or unmodified, you
+ * may:
+ *   1) Leave this header intact and distribute it under the same terms,
+ *      accompanying it with the APACHE20 and GPL20 files, or
+ *   2) Delete the Apache 2.0 clause and accompany it with the GPL2 file, or
+ *   3) Delete the GPL v2 clause and accompany it with the APACHE20 file
+ * In all cases you must keep the copyright notice intact and include a copy
+ * of the CONTRIB file.
+ *
+ * Binary distributions must follow the binary distribution requirements of
+ * either License.
+ */
+
+
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+#include <signal.h>
+
+#include <opencv2/opencv.hpp>
+
+#include <libfreenect2/libfreenect2.hpp>
+#include <libfreenect2/frame_listener_impl.h>
+#include <libfreenect2/threading.h>
+#include <libfreenect2/registration.h>
+#include <libfreenect2/packet_pipeline.h>
+
+bool protonect_shutdown = false;
+
+void sigint_handler(int s)
+{
+  protonect_shutdown = true;
+}
+
+std::string tostr(int x)
+{
+    std::stringstream str;
+    str << x;
+    return str.str();
+}
+
+
+int main(int argc, char *argv[])
+{
+  std::string program_path(argv[0]);
+  size_t executable_name_idx = program_path.rfind("Protonect");
+
+  std::string binpath = "/";
+
+  if(executable_name_idx != std::string::npos)
+  {
+    binpath = program_path.substr(0, executable_name_idx);
+  }
+
+  libfreenect2::Freenect2 freenect2;
+  libfreenect2::Freenect2Device *dev = 0;
+  libfreenect2::PacketPipeline *pipeline = 0;
+
+  if(freenect2.enumerateDevices() == 0)
+  {
+    std::cout << "no device connected!" << std::endl;
+    return -1;
+  }
+
+  std::string serial = freenect2.getDefaultDeviceSerialNumber();
+  int threshold = 128;
+
+  for(int argI = 1; argI < argc; ++argI)
+  {
+    const std::string arg(argv[argI]);
+
+    if(arg == "cpu")
+    {
+      if(!pipeline)
+        pipeline = new libfreenect2::CpuPacketPipeline();
+    }
+    else if(arg == "gl")
+    {
+#ifdef LIBFREENECT2_WITH_OPENGL_SUPPORT
+      if(!pipeline)
+        pipeline = new libfreenect2::OpenGLPacketPipeline();
+#else
+      std::cout << "OpenGL pipeline is not supported!" << std::endl;
+#endif
+    }
+    else if(arg == "cl")
+    {
+#ifdef LIBFREENECT2_WITH_OPENCL_SUPPORT
+      if(!pipeline)
+        pipeline = new libfreenect2::OpenCLPacketPipeline();
+#else
+      std::cout << "OpenCL pipeline is not supported!" << std::endl;
+#endif
+    }
+    else if (arg == "-t") {
+      std::string thresholdString (argv[argI+1]);
+
+      std::istringstream ss(argv[argI+1]);
+      int x;
+      if (!(ss >> threshold)) {
+        std::cerr << "Invalid number " << argv[argI+1] << '\n';
+      }
+
+      argI += 1;
+    }
+    else if(arg.find_first_not_of("0123456789") == std::string::npos) //check if parameter could be a serial number
+    {
+      serial = arg;
+    }
+    else
+    {
+      std::cout << "Unknown argument: " << arg << std::endl;
+    }
+  }
+
+  if(pipeline)
+  {
+    dev = freenect2.openDevice(serial, pipeline);
+  }
+  else
+  {
+    dev = freenect2.openDevice(serial);
+  }
+
+  if(dev == 0)
+  {
+    std::cout << "failure opening device!" << std::endl;
+    return -1;
+  }
+
+  signal(SIGINT,sigint_handler);
+  protonect_shutdown = false;
+
+  libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color | libfreenect2::Frame::Ir | libfreenect2::Frame::Depth);
+  libfreenect2::FrameMap frames;
+  libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4);
+
+  dev->setColorFrameListener(&listener);
+  dev->setIrAndDepthFrameListener(&listener);
+  dev->start();
+
+  std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
+  std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
+
+  libfreenect2::Registration* registration = new libfreenect2::Registration(dev->getIrCameraParams(), dev->getColorCameraParams());
+
+  std::string filename ("test_" );
+  int i = 0;
+  while(!protonect_shutdown)
+  {
+    listener.waitForNewFrame(frames);
+    libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
+    libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
+    libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
+
+    cv::imshow("rgb", cv::Mat(rgb->height, rgb->width, CV_8UC4, rgb->data));
+    //cv::imshow("ir", cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 20000.0f);
+    //cv::imshow("depth", cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0f);
+
+    registration->apply(rgb, depth, &undistorted, &registered);
+
+    //cv::imshow("undistorted", cv::Mat(undistorted.height, undistorted.width, CV_32FC1, undistorted.data) / 4500.0f);
+    //cv::imshow("registered", cv::Mat(registered.height, registered.width, CV_8UC4, registered.data));
+
+    if (i > 10) {
+      i = 0;
+      for (int j=0; j<=10; j++) {
+        std::string jIndex = tostr(j);
+        if(remove((filename + jIndex + ".jpg").c_str()) != 0) {
+          perror( "Error deleting file" );
+        } else {
+          puts( "File successfully deleted" );
+        }
+      }
+    }
+
+
+    cv::Mat undistortedFrame = cv::Mat(undistorted.height, undistorted.width, CV_32FC1, undistorted.data) / 4500.0f;
+    cv::Mat img_bw;
+    undistortedFrame.convertTo(img_bw, CV_8UC4, 255);
+    cv::Mat thresh = img_bw > threshold;
+
+    cv::imshow("undistorted", thresh);
+
+
+    std::string index = tostr(i);
+    cv::imwrite((filename + index + ".jpg").c_str(), thresh);
+    i += 1;
+
+    int key = cv::waitKey(1);
+    protonect_shutdown = protonect_shutdown || (key > 0 && ((key & 0xFF) == 27)); // shutdown on escape
+    if (!protonect_shutdown) {
+      std::cout << "key=" << (key & 0xFF) << std::endl;
+      if (key == 82) {
+        threshold += 1;
+        std::cout << "increasing threshold" << std::endl;
+      } else if (key == 83) {
+        threshold -= 1;
+        std::cout << "decreasing threshold" << std::endl;
+      }
+    }
+
+    listener.release(frames);
+    libfreenect2::this_thread::sleep_for(libfreenect2::chrono::milliseconds(1000));
+  }
+
+  // TODO: restarting ir stream doesn't work!
+  // TODO: bad things will happen, if frame listeners are freed before dev->stop() :(
+  dev->stop();
+  dev->close();
+
+  delete registration;
+
+  return 0;
+}
